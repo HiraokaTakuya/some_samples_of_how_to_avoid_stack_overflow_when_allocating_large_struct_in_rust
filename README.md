3636a1e3c3a77ee0ea5265b8702cabe70811e423
HiraokaTakuya <hiraoka64@gmail.com>
2021-02-12 02:06:05 +0900

    Add items.

---
+++ README.md
@@ -0,0 +1,14 @@
+# some_samples_of_how_to_avoid_stack_overflow_when_allocating_large_struct_in_rust
+
+Some samples of how to avoid stack overflow when allocating large struct in Rust.
+
+## Rust toolchain
+
+nightly
+
+## License
+
+This is distributed under the terms of both the MIT license and the
+Apache License (Version 2.0).
+
+See [LICENSE-APACHE](LICENSE-APACHE) and [LICENSE-MIT](LICENSE-MIT) for details.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,98 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(new_uninit)]
+
+const N: usize = 512;
+
+#[derive(Debug, Clone, Copy)]
+struct X<T: Copy>([T; N]);
+
+impl<T: Copy> X<T> {
+    fn new(item: T) -> Self {
+        Self([item; N])
+    }
 }
+
+type A = X<i8>;
+type B = X<A>;
+type C = X<B>;
+
+// This function causes stack overflow.
+#[allow(dead_code)]
+fn new() -> C {
+    C::new(B::new(A::new(1)))
+}
+
+// This function causes stack overflow.
+#[allow(dead_code)]
+fn new_with_box() -> Box<C> {
+    Box::new(C::new(B::new(A::new(1))))
+}
+
+// This function causes compile error. Vec<T> does not has 'Copy'.
+// fn new_with_vec() {
+//     #[derive(Clone, Copy)]
+//     struct X<T: Copy>(Vec<T>);
+//
+//     impl<T: Copy> X<T> {
+//         fn new(item: T) -> Self {
+//             Self(vec![item; N])
+//         }
+//     }
+//
+//     type A = X<i32>;
+//     type B = X<A>;
+//     type C = X<B>;
+//     let _ = C::new(B::new(A::new(1)));
+// }
+
+#[allow(dead_code)]
+fn new_with_box_new_uninit() -> Box<C> {
+    // Box::new_uninit() is a nightly-only API.
+    let mut c = Box::<C>::new_uninit();
+    unsafe {
+        let c_ptr = c.as_mut_ptr();
+        for b in &mut (*c_ptr).0 {
+            for a in &mut b.0 {
+                *a = A::new(1);
+            }
+        }
+        c.assume_init()
+    }
+}
+
+#[allow(dead_code)]
+fn new_with_ptr() -> Box<C> {
+    unsafe {
+        let c_ptr = std::alloc::alloc(std::alloc::Layout::new::<C>()) as *mut C;
+        for b in &mut (*c_ptr).0 {
+            for a in &mut b.0 {
+                *a = A::new(1);
+            }
+        }
+        Box::from_raw(c_ptr)
+    }
+}
+
+// This function causes stack overflow.
+#[allow(dead_code)]
+fn new_with_thread() -> C {
+    const STACK_SIZE: usize = 512 * 1024 * 1024;
+    std::thread::Builder::new()
+        .stack_size(STACK_SIZE)
+        .spawn(|| C::new(B::new(A::new(1))))
+        .unwrap()
+        .join()
+        .unwrap()
+}
+
+#[allow(dead_code)]
+fn new_with_box_in_thread() -> Box<C> {
+    const STACK_SIZE: usize = 512 * 1024 * 1024;
+    std::thread::Builder::new()
+        .stack_size(STACK_SIZE)
+        .spawn(|| Box::new(C::new(B::new(A::new(1)))))
+        .unwrap()
+        .join()
+        .unwrap()
+}
+
+fn main() {}
